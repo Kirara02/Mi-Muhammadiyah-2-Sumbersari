4c710542ac526df647c5d0c9cf844b384bed4823
Fathul Hidayat <100482319+Kirara02@users.noreply.github.com>
2024-05-09 19:09:27 +0700

    initial push

---
+++ README.txt
@@ -0,0 +1,14 @@
+untuk content gambar, bisa di pilih dari internet / di masukan sendiri di file contents.json
+
+silahkan masukkan ke dalam folder images lalu buka contents.json, lihat pada visiMisi, disitu untuk memilih gambar dalam folder images caranya adalah images/namagambar.jpg/png/webp/jpeg,
+untuk file yang ada di internet, bisa langsung di paste saja alamat url nya, seperti ada backgroundHome
+
+logo -> untuk gambar di atas kanan navigasi
+backgroundHome -> untuk background pada header home jumbotron awal
+visiMisi -> untuk gambar pada section visi misi sekolah
+teachers -> untuk memasukkan staf / guru sekolah yang bersangkutan
+
+untuk teachers ada 3 atribute yang harus di isi yaitu foto guru, nama, serta peran / pekerjaan
+di list pertama saya memberikan contoh bagaimana cara mengisi serta apa yang akan terlihat di website nya
+
+pastikan untuk menghapus koma paling akhir pada list terakhir
---
+++ index.js
@@ -0,0 +1,99 @@
+(async () => await fetch("contents.json").then(res => res.json()).then(res => {
+    const nav = document.getElementById("nav");
+    const home = document.getElementById("home");
+    const schoolLogo = document.getElementById("schoolLogo");
+    const schoolName = document.getElementById("schoolName");
+    
+    const teacherContainer = document.getElementById("teacherContainer");
+    const prevTeacherBtn = document.getElementById("prevTeacherBtn");
+    const nextTeacherBtn = document.getElementById("nextTeacherBtn");
+    const visiMisiImage = document.getElementById("visiMisiImage");
+
+    if (res.logo) {
+        schoolName.remove();
+        schoolLogo.src = res.logo;
+    } else {
+        schoolLogo.remove();
+    }
+
+    home.style.background = `linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('${res.backgroundHome}') no-repeat center`
+    home.style.backgroundSize = "cover"
+
+    visiMisiImage.src = res.visiMisi;
+
+    let el = "";
+
+    res.teachers.forEach(teacher => {
+        el += `
+        <div class="pop-up group teacher-wrapper">
+            <div class="group-hover:opacity-100 teacher-info">
+                <h1 class="group-hover:translate-y-0 group-hover:opacity-100">${teacher.name}</h1>
+                <p class="group-hover:translate-y-0 group-hover:opacity-100">${teacher.role}</p>
+            </div>
+            <img src="${teacher.image}" alt="${teacher.name}" class="teacher-image">
+        </div>
+        `        
+    });
+
+    teacherContainer.innerHTML = el;
+
+    window.addEventListener("load", async () => {
+        gsap.to("#loader", {
+            opacity: 0,
+            duration: 1,
+            onComplete: () => {
+                document.getElementById("loader").remove();
+                
+                const observer = new IntersectionObserver(entries => {
+                    entries.forEach(v => {
+                        if (v.isIntersecting) {
+                            if (v.target.id == "home") {
+                                nav.classList.add("nav-transparent")
+                                nav.classList.remove("nav-white")
+                            } else {
+                                nav.classList.add("nav-white")
+                                nav.classList.remove("nav-transparent")
+                                document.getElementById(`${v.target.id}Nav`).classList.add("bg-primary", "text-white");
+                            }
+        
+                            gsap.to(`#${v.target.id} .pop-up`, {
+                                y: 0,
+                                opacity: 1,
+                                stagger: .25
+                            })
+        
+                        } else {
+                            document.getElementById(`${v.target.id}Nav`).classList.remove("bg-primary", "text-white");
+                        }
+                    });
+                }, {
+                    threshold: 0.5
+                });
+        
+                document.querySelectorAll(".section").forEach(v => {
+                    observer.observe(v);
+                });
+        
+                gsap.to("#nav .pop-up", {
+                    y: 0,
+                    opacity: 1,
+                    stagger: .1
+                })
+        
+                prevTeacherBtn.addEventListener("click", () => {
+                    teacherContainer.scrollTo({
+                        left: teacherContainer.scrollLeft - 500
+                    })
+                });
+                
+                nextTeacherBtn.addEventListener("click", () => {
+                    teacherContainer.scrollTo({
+                        left: teacherContainer.scrollLeft + 500
+                    })
+                });
+                
+                document.getElementById("copyright").innerHTML = `&copy; ${new Date().getFullYear()} All Rights Reserved By MI Muhammadiyah 2 Sumbersari`
+            }
+        });
+    });
+}))();
